30667472ac0f2689cb67f00d7ec1c3ff3ca4b37e
DiegoAldair10 <diegotello211098@gmail.com>
2020-08-13 18:31:39 -0500

    go

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,30 @@ import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { SliderComponent } from './slider/slider.component';
 import { HttpClientModule } from '@angular/common/http';
-/*NG Boostratp */
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ProductosComponent } from './productos/productos.component';
 import { FormComponent } from './productos/form.component';
+/*NG Boostratp */
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule , Routes } from '@angular/router';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { FormsModule} from '@angular/forms';
+
+// /*Material*/
+// import { MatMomentDateModule } from '@angular/material-moment-adapter';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/productos', pathMatch: 'full'},
-  {path: 'productos', component: ProductosComponent},
+  {path: 'productos', component: ProductosComponent}, 
+  {path: 'productos/page/:page', component: ProductosComponent},
+  {path: 'productos/form', component: FormComponent},
+  {path: 'productos/form/:id', component: FormComponent}
+
 ];
 
 
@@ -36,7 +49,13 @@ const routes: Routes = [
     BrowserModule,
     RouterModule.forRoot(routes),
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+     FormsModule
+    // MatDatepickerModule,
+    // MatMomentDateModule,
+    // MatAutocompleteModule,
+    // MatInputModule,
+    // MatFormFieldModule
 
   ],
   providers: [],
--- src/app/productos/form.component.ts
+++ src/app/productos/form.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from './producto';
+import { ProductoService } from './producto.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  private producto: Producto = new Producto();
+  titulo: string = 'Crear Producto';
+  errores: string[];
 
-  constructor() { }
+  constructor(
+    private productoService: ProductoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let id = +params.get('id');
+      if (id) {
+        this.productoService
+          .getProducto(id)
+          .subscribe((producto) => (this.producto = producto));
+      }
+    });
   }
 
+  create(): void {
+    console.log(this.producto);
+    this.productoService.create(this.producto).subscribe(
+      (producto) => {
+        this.router.navigate(['/productos']);
+        Swal.fire(
+          'Nuevo producto',
+          `El producto ${producto.nombre} ha sido creado correctamente`,
+          'success'
+        );
+      },
+      (err) => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error el backend:' + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+
+  update(): void {
+    console.log(this.producto);
+    this.productoService.update(this.producto).subscribe(
+      (json) => {
+        this.router.navigate(['/productos']);
+        Swal.fire(
+          'Producto Actualizado',
+          ` ${json.mensaje}: ${json.producto.nombre}`,
+          'success'
+        );
+      },
+      (err) => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error el backend:' + err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
 }
