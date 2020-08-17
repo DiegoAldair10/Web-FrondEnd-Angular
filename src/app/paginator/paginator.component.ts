04fc9fad53b01d1296f4fd68453e7366fdece3c7
DiegoAldair10 <diegotello211098@gmail.com>
2020-08-16 19:36:36 -0500

    Go

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -30,10 +30,10 @@ export class PaginatorComponent implements OnInit {
   }
 
   private initPaginator(): void {
-    this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
-    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+    this.desde = Math.min(Math.max(1, this.paginador.number - 8), this.paginador.totalPages - 9);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 8), 9);
 
-    if (this.paginador.totalPages > 5) {
+    if (this.paginador.totalPages > 8) {
       this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((_valor, indice) => indice + this.desde);
     } else {
       this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
